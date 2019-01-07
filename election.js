8f01a971a00fef166d8b9da1e49bd26f67654077
victoriak94 <vkamila6@gmail.com>
2019-01-07 15:00:53 -0500

    Save election results in variable. Make get request to election results. Iterate through data and save candidate results in variable. Create list element to display to candidate results. Append candidate results to election results element. Complete assignment

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  const electionResults = document.querySelector('#electionResults');
+
+  const resultsRequest = axios({
+    method: 'GET',
+    url: 'https://bb-election-api.herokuapp.com/',
+    responseType: 'text'
+  }).then( (response) => {
+    response.data.candidates.forEach((candidate) => {
+      const candidateResult = document.createElement('li');
+      candidateResult.innerHTML = (`${candidate.name}: ${candidate.votes} votes`);
+      electionResults.appendChild(candidateResult);
+    });
+  });
 
 });
